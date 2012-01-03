e34a4719281d1a82a124be633ddde3df53e6ecce
AlfieC <alfeh@me.com>
2012-01-03 17:09:56 +0000

    Made messages a bit clearer to non-English users.

--- src/org/cain/cmdbin/CommandBin.java
+++ src/org/cain/cmdbin/CommandBin.java
@@ -48,7 +48,7 @@ public class CommandBin extends JavaPlugin{
 	
 	public String PLAYER_OFFLINE = "This player is currently offline.";
 	public String NULL_PERMISSION = "You have no permission to use this command.";
-	public String CONSOLE_SENDER = "You must be in-game to use this command.";
+	public String CONSOLE_SENDER = "You mustn't be on console, but in game to use this command.";
 	public static PermissionHandler permissionHandler;
 	public static CommandBin cmdbin;
 	public static Configuration cfg;
